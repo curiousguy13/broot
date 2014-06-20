1baa3fb5de5a5f0b8d03b1bf81b2d1340e7a6a66
kunal arora <kunalarora.135@gmail.com>
2014-06-20 17:46:16 +0530

    py3k changes

--- broot/root.py
+++ broot/root.py
@@ -20,7 +20,7 @@ import json
 import os
 import signal
 import shutil
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 from subprocess import check_call, call, check_output
 
 import wget
@@ -61,9 +61,10 @@ class Root:
 
     def _compute_path(self):
         path_hash = hashlib.sha1()
-        path_hash.update(self._config_path)
+        path_hash.update(self._config_path.encode('utf-8'))
 
         base64_hash = base64.b64encode(path_hash.digest())
+        base64_hash = base64_hash.decode('utf-8')
         base64_hash = base64_hash.replace("+", "0")
         base64_hash = base64_hash.replace("/", "0")
 
@@ -77,7 +78,7 @@ class Root:
     def _compute_mounts(self):
         mounts = collections.OrderedDict()
 
-        for source_path, dest_path in self._get_user_mounts().items():
+        for source_path, dest_path in list(self._get_user_mounts().items()):
             full_dest_path = os.path.join(self.path, dest_path)
             mounts[os.path.abspath(source_path)] = full_dest_path
 
@@ -116,7 +117,7 @@ class Root:
 
         mounted = self._get_mounted()
 
-        for source_path, dest_path in self._mounts.items():
+        for source_path, dest_path in list(self._mounts.items()):
             if dest_path not in mounted:
                 if os.path.exists(dest_path):
                     check_call(["mount", "--bind", source_path, dest_path])
@@ -148,16 +149,16 @@ class Root:
 
                 if chroot:
                     try:
-                        print "Killing %s" % pid
+                        print("Killing %s" % pid)
                         os.kill(int(pid), signal.SIGTERM)
-                    except OSError, e:
-                        print "Failed: %s" % e
+                    except OSError as e:
+                        print("Failed: %s" % e)
 
     def deactivate(self):
         self._kill_processes()
 
         mounted = self._get_mounted()
-        for mount_path in reversed(self._mounts.values()):
+        for mount_path in reversed(list(self._mounts.values())):
             if mount_path in mounted:
                 check_call(["umount", mount_path])
 
@@ -177,7 +178,7 @@ class Root:
         self._builder.update_packages()
 
         flat_packages = []
-        for group in self._config["packages"].values():
+        for group in list(self._config["packages"].values()):
             for package in group:
                 if package not in flat_packages:
                     flat_packages.append(package)
@@ -278,9 +279,9 @@ class Root:
                                      prebuilt_name)
 
         try:
-            last = urllib2.urlopen(last_url).read().strip()
+            last = urllib.request.urlopen(last_url).read().strip()
         except:
-            print "Failed to download %s" % last_url
+            print("Failed to download %s" % last_url)
             raise
 
         try:
@@ -304,7 +305,7 @@ class Root:
 
         os.unlink(tar_filename)
 
-        print ""
+        print("")
 
         if result != 0:
             return False
@@ -357,7 +358,7 @@ class Root:
                     env[name] = os.environ[name]
 
             env_string = ""
-            for name, value in env.items():
+            for name, value in list(env.items()):
                 env_string += "%s=%s " % (name, value)
 
             result = call("%s %s /usr/bin/env -i %s /bin/bash -lc \"%s\"" %
@@ -420,7 +421,7 @@ class Root:
             to_chown.append(bashrc_path)
 
         try:
-            for path in self._get_user_mounts().values():
+            for path in list(self._get_user_mounts().values()):
                 full_path = os.path.join(self.path, path)
                 os.makedirs(full_path)
                 to_chown.append(full_path)
